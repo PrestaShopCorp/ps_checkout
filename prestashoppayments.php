e529e33bdac320b04801c8ab2a210ded85694567
jGissinger <julien.gissinger@prestashop.com>
2019-02-14 15:11:33 +0100

    Add payment options (paypal and hosted fields) at checkout

--- prestashoppayments.php
+++ prestashoppayments.php
@@ -24,16 +24,23 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
+use PrestaShop\PrestaShop\Core\Payment\PaymentOption;
+
 if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class prestashoppayments extends Module
+class Prestashoppayments extends PaymentModule
 {
+    public $hookList = [
+        'paymentOptions',
+        'paymentReturn'
+    ];
+
     public function __construct()
     {
         $this->name = 'prestashoppayments';
-        $this->tab = 'payment';
+        $this->tab = 'payments_gateways';
         $this->version = '1.0.0';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
@@ -49,5 +56,82 @@ class prestashoppayments extends Module
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall this module?');
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    }
+
+    public function install()
+    {
+        return parent::install() && $this->registerHook($this->hookList);
+    }
+
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+
+    public function hookPaymentOptions($params)
+    {
+        if (!$this->active) {
+            return;
+        }
+
+        if (!$this->checkCurrency($params['cart'])) {
+            return;
+        }
+
+        $payment_options = [
+            $this->getPaypalPaymentOption(),
+            $this->getHostedFieldPaymentOption()
+        ];
+
+        return $payment_options;
+    }
+
+    public function getPaypalPaymentOption()
+    {
+        $paypalPaymentOption = new PaymentOption();
+        $paypalPaymentOption->setCallToActionText($this->l(''))
+                            ->setAction($this->context->link->getModuleLink($this->name, 'validation', array(), true))
+                            ->setInputs([
+                                'token' => [
+                                    'name' =>'token',
+                                    'type' =>'hidden',
+                                    'value' =>'12345689',
+                                ],
+                            ])
+                            ->setLogo(Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/views/img/mastercard.jpg'));
+
+        return $paypalPaymentOption;
+    }
+
+    public function getHostedFieldPaymentOption()
+    {
+        $offlineOption = new PaymentOption();
+        $offlineOption->setCallToActionText($this->l('Hosted fields'))
+                      ->setAction('https://payment-webinit.sogenactif.com/paymentInit')
+                      ->setForm($this->generateHostedFieldsForm())
+                      ->setLogo(Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/views/img/mastercard.jpg'));
+
+        return $offlineOption;
+    }
+
+    public function generateHostedFieldsForm()
+    {
+        return $this->context->smarty->fetch('module:prestashoppayments/views/templates/front/payment_form.tpl');
+    }
+
+    public function checkCurrency($cart)
+    {
+        $currency_order = new Currency($cart->id_currency);
+        $currencies_module = $this->getCurrency($cart->id_currency);
+
+        if (is_array($currencies_module)) {
+            foreach ($currencies_module as $currency_module) {
+                if ($currency_order->id == $currency_module['id_currency']) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
