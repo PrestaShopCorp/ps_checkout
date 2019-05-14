5bf0fcfb8a63204d46dc99503ec5de411d3467f0
v4lux <julien.gissinger@prestashop.com>
2019-05-14 09:28:49 +0200

    Return directly a promise instead of wrapping axios call in another promise

--- _dev/store/modules/firebase/actions.js
+++ _dev/store/modules/firebase/actions.js
@@ -3,50 +3,48 @@ import {ajax} from '@/requests/ajax.js';
 
 export default {
   login({commit}, payload) {
-    return new Promise((resolve, reject) => {
-      ajax({
-        action: 'SignIn',
-        data: {
-          email: payload.email,
-          password: payload.password,
-        },
-      }).then((user) => {
-        if (user.error) {
-          reject(user);
-        } else {
-          commit(types.UPDATE_ACCOUNT, {
-            email: user.email,
-            idToken: user.idToken,
-            localId: user.localId,
-            refreshToken: user.refreshToken,
-          });
-          resolve(user);
-        }
+    return ajax({
+      action: 'SignIn',
+      data: {
+        email: payload.email,
+        password: payload.password,
+      },
+    }).then((user) => {
+      if (user.error) {
+        return Promise.reject(user);
+      }
+
+      commit(types.UPDATE_ACCOUNT, {
+        email: user.email,
+        idToken: user.idToken,
+        localId: user.localId,
+        refreshToken: user.refreshToken,
       });
+
+      return Promise.resolve(user);
     });
   },
 
   signup({commit}, payload) {
-    return new Promise((resolve, reject) => {
-      ajax({
-        action: 'SignUp',
-        data: {
-          email: payload.email,
-          password: payload.password,
-        },
-      }).then((user) => {
-        if (user.error) {
-          reject(user);
-        } else {
-          commit(types.UPDATE_ACCOUNT, {
-            email: user.email,
-            idToken: user.idToken,
-            localId: user.localId,
-            refreshToken: user.refreshToken,
-          });
-          resolve(user);
-        }
+    ajax({
+      action: 'SignUp',
+      data: {
+        email: payload.email,
+        password: payload.password,
+      },
+    }).then((user) => {
+      if (user.error) {
+        return Promise.reject(user);
+      }
+
+      commit(types.UPDATE_ACCOUNT, {
+        email: user.email,
+        idToken: user.idToken,
+        localId: user.localId,
+        refreshToken: user.refreshToken,
       });
+
+      return Promise.resolve(user);
     });
   },
 };
