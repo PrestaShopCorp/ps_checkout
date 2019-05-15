ca0ea6a5f89dc1253a11c955dfe9d80ec2fde3b5
David Piatek <david.piatek@prestashop.com>
2019-05-15 11:11:13 +0200

    Add CD for test on Ready

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+FILEPATH=$1
+ENV=$2
+gcloud container clusters get-credentials --project="prestashop-ready-$ENV" --zone="europe-west1-d" "prestashop-ready-cluster"
+
+NAME=$(kubectl get pods --namespace=$ENV-shops -l shop=dep-checkout -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+
+kubectl cp /workspace/$FILEPATH $ENV-shops/$NAME:/
+kubectl exec -t --namespace=$ENV-shops $NAME -- bash -c \
+    '
+    sudo -u presthost -H bash -c "unzip -o /$FILEPATH -d /presthost/userland/modules > /dev/null 2>&1;";
+    rm -f /'"${FILEPATH}"';
+    /presthost/core/bin/console  prestashop:module uninstall ps_checkout || true;
+    /presthost/core/bin/console  prestashop:module install ps_checkout;
+    '
