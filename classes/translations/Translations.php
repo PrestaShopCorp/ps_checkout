da9a0eb9a35e40dbe10e34da7aaf8ab0f406c65b
v4lux <julien.gissinger@prestashop.com>
2019-06-26 12:26:18 +0200

    Add translation system to Accounts page

--- classes/translations/Translations.php
+++ classes/translations/Translations.php
@@ -59,23 +59,31 @@ class Translations
             'general' => array(
                 'save' => $this->module->l('Save'),
             ),
-            'auth' => array(
-                'test' => $this->module->l('test'),
+            'pages' => array(
+                'accounts' => array(
+                    'approvalPending' => $this->module->l('Approval pending'),
+                    'waitingEmail' => $this->module->l('We are waiting for email confirmation… Check your inbox to finalize creation.'),
+                    'didntReceiveEmail' => $this->module->l('Didn’t receive any confirmation email?'),
+                    'sendEmailAgain' => $this->module->l('Send email again'),
+                    'documentNeeded' => $this->module->l('Documents needed'),
+                    'additionalDocumentsNeeded' => $this->module->l('We need additional documents to complete our background check. Please prepare the following documents'),
+                    'photoIds' => $this->module->l('Photo IDs, such as driving licence, for all beneficial owners'),
+                    'uploadFile' => $this->module->l('Upload file'),
+                    'undergoingCheck' => $this->module->l('Your case is currently undergoing necessary background check'),
+                    'severalDays' => $this->module->l('This can take several days. If further information is needed, you will be notified.'),
+                    'youCanProcess' => $this->module->l('You can process'),
+                    'upTo' => $this->module->l('up to $500'),
+                    'transactionsUntil' => $this->module->l('in card transactions until your account is fully approved to accept card payment.'),
+                    'approvalPendingLink' => $this->module->l('Approval pending FAQs'),
+                    'accountDeclined' => $this->module->l('Account declined'),
+                    'cannotProcessCreditCard' => $this->module->l('We cannot process credit card payments for you at the moment. You can reapply after 90 days, in the meantine you can accept orders via PayPal.'),
+                    'accountDeclinedLink' => $this->module->l('Account declined FAQs'),
+                ),
             ),
-            'customize' => array(
-                'test1' => $this->module->l('test2'),
-            ),
-            'manage' => array(
-                'test' => $this->module->l('test'),
-            ),
-            'advanced' => array(
-                'test' => $this->module->l('test'),
-            ),
-            'fees' => array(
-                'test' => $this->module->l('test'),
-            ),
-            'help' => array(
-                'test' => $this->module->l('test'),
+            'panel' => array(
+                'account-list' => array(
+                    'test' => 'test',
+                ),
             ),
         );
 
