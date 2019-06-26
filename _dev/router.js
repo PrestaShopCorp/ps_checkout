3409d893a45fd601cb5104554b7384e7c5a3dc2c
v4lux <julien.gissinger@prestashop.com>
2019-06-26 10:26:15 +0200

    Adding new routes

--- _dev/router.js
+++ _dev/router.js
@@ -1,15 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import PaypalAuth from '@/pages/PaypalAuth.vue';
 import Signup from '@/pages/Signup.vue';
 import Login from '@/pages/Login.vue';
 import Customize from '@/pages/Customize.vue';
 import Accounts from '@/pages/Accounts.vue';
-// import Manage from '@/pages/Manage.vue';
-// import Advanced from '@/pages/Advanced.vue';
-// import Fees from '@/pages/Fees.vue';
-// import Help from '@/pages/Help.vue';
+import Activity from '@/pages/Activity.vue';
+import Advanced from '@/pages/Advanced.vue';
+import Fees from '@/pages/Fees.vue';
+import Help from '@/pages/Help.vue';
 
 Vue.use(Router);
 
@@ -35,34 +34,29 @@ export default new Router({
       component: Signup,
     },
     {
-      path: '/authentication/paypal',
-      name: 'PaypalAuth',
-      component: PaypalAuth,
-    },
-    {
       path: '/customize',
       name: 'Customize',
       component: Customize,
     },
-    // {
-    //   path: '/manage',
-    //   name: 'Manage',
-    //   component: Manage,
-    // },
-    // {
-    //   path: '/advanced',
-    //   name: 'Advanced',
-    //   component: Advanced,
-    // },
-    // {
-    //   path: '/fees',
-    //   name: 'Fees',
-    //   component: Fees,
-    // },
-    // {
-    //   path: '/help',
-    //   name: 'Help',
-    //   component: Help,
-    // },
+    {
+      path: '/activity',
+      name: 'Activity',
+      component: Activity,
+    },
+    {
+      path: '/advanced',
+      name: 'Advanced',
+      component: Advanced,
+    },
+    {
+      path: '/fees',
+      name: 'Fees',
+      component: Fees,
+    },
+    {
+      path: '/help',
+      name: 'Help',
+      component: Help,
+    },
   ],
 });
