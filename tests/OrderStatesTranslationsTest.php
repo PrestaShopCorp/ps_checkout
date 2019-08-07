bfeb42d8777eb0966679fcce34cb8722dc778f35
apacios <adrien.pacios@prestashop.com>
2019-08-07 15:01:32 +0200

    Add units tests for Order States Translations

---
+++ tests/OrderStatesTranslationsTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * 2007-2019 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author PrestaShop SA <contact@prestashop.com>
+ *  @copyright  2007-2019 PrestaShop SA
+ *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ **/
+use PHPUnit\Framework\TestCase;
+use PrestaShop\Module\PrestashopCheckout\Translations\OrderStatesTranslations;
+
+class OrderStatesTranslationsTest extends TestCase
+{
+    /**
+     * @dataProvider getLanguageText
+     */
+    public function testOrderStatesGetLanguageText($resultExpect, $dataToValidate)
+    {
+        $this->assertSame(
+            $resultExpect,
+            (new OrderStatesTranslations())->getTranslations($dataToValidate)
+        );
+    }
+
+    public function getLanguageText()
+    {
+        return array(
+            array(
+                array(
+                    'PS_CHECKOUT_STATE_WAITING_PAYPAL_PAYMENT' => OrderStatesTranslations::PS_CHECKOUT_STATE_WAITING_PAYPAL_PAYMENT['en'],
+                    'PS_CHECKOUT_STATE_WAITING_CREDIT_CARD_PAYMENT' => OrderStatesTranslations::PS_CHECKOUT_STATE_WAITING_CREDIT_CARD_PAYMENT['en'],
+                    'PS_CHECKOUT_STATE_WAITING_LOCAL_PAYMENT' => OrderStatesTranslations::PS_CHECKOUT_STATE_WAITING_LOCAL_PAYMENT['en'],
+                    'PS_CHECKOUT_STATE_AUTHORIZED' => OrderStatesTranslations::PS_CHECKOUT_STATE_AUTHORIZED['en'],
+                    'PS_CHECKOUT_STATE_PARTIAL_REFUND' => OrderStatesTranslations::PS_CHECKOUT_STATE_PARTIAL_REFUND['en'],
+                    'PS_CHECKOUT_STATE_WAITING_CAPTURE' => OrderStatesTranslations::PS_CHECKOUT_STATE_WAITING_CAPTURE['en'],
+                ),
+                'en',
+            ),
+            array(
+                array(
+                    'PS_CHECKOUT_STATE_WAITING_PAYPAL_PAYMENT' => OrderStatesTranslations::PS_CHECKOUT_STATE_WAITING_PAYPAL_PAYMENT['en'],
+                    'PS_CHECKOUT_STATE_WAITING_CREDIT_CARD_PAYMENT' => OrderStatesTranslations::PS_CHECKOUT_STATE_WAITING_CREDIT_CARD_PAYMENT['en'],
+                    'PS_CHECKOUT_STATE_WAITING_LOCAL_PAYMENT' => OrderStatesTranslations::PS_CHECKOUT_STATE_WAITING_LOCAL_PAYMENT['en'],
+                    'PS_CHECKOUT_STATE_AUTHORIZED' => OrderStatesTranslations::PS_CHECKOUT_STATE_AUTHORIZED['en'],
+                    'PS_CHECKOUT_STATE_PARTIAL_REFUND' => OrderStatesTranslations::PS_CHECKOUT_STATE_PARTIAL_REFUND['en'],
+                    'PS_CHECKOUT_STATE_WAITING_CAPTURE' => OrderStatesTranslations::PS_CHECKOUT_STATE_WAITING_CAPTURE['en'],
+                ),
+                'notIsoCode',
+            ),
+        );
+    }
+}
