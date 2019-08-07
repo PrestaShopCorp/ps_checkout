7d135cbc9c09f57ede4b3d5b02b43f7c9123314c
v4lux <julien.gissinger@prestashop.com>
2019-08-07 18:24:56 +0200

    Bump version to 1.0.6

--- ps_checkout.php
+++ ps_checkout.php
@@ -77,7 +77,7 @@ class ps_checkout extends PaymentModule
     {
         $this->name = 'ps_checkout';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.5';
+        $this->version = '1.0.6';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
