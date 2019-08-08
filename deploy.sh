63e009114317ab6063b3164f03d09efe549c037b
Jacob Dharandas <jacob.dharandas@prestashop.com>
2019-08-08 17:16:26 +0200

    i - Added deploy for 6 shops + remove uninstall (module)

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,6 @@ function deploy() {
     kubectl cp /workspace/$FILEPATH $ENV-shops/$1:/
     kubectl exec -t --namespace=$ENV-shops $1 -- bash -c \
         "
-        /presthost/core/bin/console  prestashop:module uninstall ps_checkout || true;	
         sudo -u presthost -H bash -c \"unzip -o /$FILEPATH -d /presthost/userland/modules > /dev/null 2>&1;\";
         rm -f /${FILEPATH};
         /presthost/core/bin/console  prestashop:module install ps_checkout;
@@ -24,6 +23,12 @@ function deploy() {
 if [[ "$ENV" == "prod" ]]; then
     deploy $(kubectl get pods --namespace=$ENV-shops -l shop=dep-mugshot -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
     deploy $(kubectl get pods --namespace=$ENV-shops -l shop=dep-kjffkjjhjgklgjh -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+    deploy $(kubectl get pods --namespace=$ENV-shops -l shop=dep-sweetaxo -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+    deploy $(kubectl get pods --namespace=$ENV-shops -l shop=dep-art-et-signaletique -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+    deploy $(kubectl get pods --namespace=$ENV-shops -l shop=dep-leeroycustomshop -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+    deploy $(kubectl get pods --namespace=$ENV-shops -l shop=dep-ah5-sailing -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+    deploy $(kubectl get pods --namespace=$ENV-shops -l shop=dep-la-vie-tout-en-bio -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+    deploy $(kubectl get pods --namespace=$ENV-shops -l shop=dep-britneystore -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 elif [[ "$ENV" == "qa" ]]; then
     deploy $(kubectl get pods --namespace=$ENV-shops -l shop=dep-checkout -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 else
