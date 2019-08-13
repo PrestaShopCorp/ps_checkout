498f63f6bce2a774b6891b08424cd8849590832f
apacios <adrien.pacios@prestashop.com>
2019-08-13 15:12:17 +0200

    Travis fix

--- translations/de.php
+++ translations/de.php
@@ -1,4 +1,5 @@
 <?php
+
 global $_MODULE;
 $_MODULE = array();
 $_MODULE['<{ps_checkout}prestashop>hostedfieldserrors_0fdfcaa100b861173fa706fed28ff9f8'] = 'Ungültiges Format, prüfen Sie Ihre Bankdaten und versuchen Sie es erneut';
