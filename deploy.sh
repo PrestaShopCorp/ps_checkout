c17e2cd971375167981d5ffb4b7b20b7fd1d85bd
Jacob Dharandas <jacob.dharandas@prestashop.com>
2019-08-13 10:09:58 +0200

    i - Added mkdir img tmp

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ function deploy() {
     kubectl cp /workspace/$FILEPATH $ENV-shops/$1:/
     kubectl exec -t --namespace=$ENV-shops $1 -- bash -c \
         "
+        sudo -u presthost -H bash -c \"mkdir -p /presthost/core/img/tmp &>/dev/null;\";
         sudo -u presthost -H bash -c \"unzip -o /$FILEPATH -d /presthost/userland/modules > /dev/null 2>&1;\";
         rm -f /${FILEPATH};
         sudo -u presthost --preserve-env -H bash -c '/presthost/core/bin/console  prestashop:module install ps_checkout';
