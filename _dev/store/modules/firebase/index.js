05680cd0aec23125f70df4d30c94abe8d5b9d72c
v4lux <julien.gissinger@prestashop.com>
2019-08-21 15:22:55 +0200

    Remove useless actions and mutations in the firebase module - handled now by sso

--- _dev/store/modules/firebase/index.js
+++ _dev/store/modules/firebase/index.js
@@ -1,12 +1,7 @@
-import actions from './actions';
-import mutations from './mutations';
-
 const store = JSON.parse(global.store);
 
 const state = store.firebase;
 
 export default {
   state,
-  mutations,
-  actions,
 };
