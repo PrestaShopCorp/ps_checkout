6101814c6efde0d2077f067624272a86669927b8
apacios <adrien.pacios@prestashop.com>
2019-10-07 17:45:24 +0200

    Bump to 1.2.1

--- ps_checkout.php
+++ ps_checkout.php
@@ -82,7 +82,7 @@ class ps_checkout extends PaymentModule
     {
         $this->name = 'ps_checkout';
         $this->tab = 'payments_gateways';
-        $this->version = '1.2';
+        $this->version = '1.2.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
