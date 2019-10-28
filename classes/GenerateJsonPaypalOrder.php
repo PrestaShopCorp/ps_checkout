701c29aa879906387c2074e358cd4a58f7a3640b
v4lux <julien.gissinger@prestashop.com>
2019-10-28 14:34:17 +0100

    Use mb_substr instead of substr to handle utf-8 string

--- classes/GenerateJsonPaypalOrder.php
+++ classes/GenerateJsonPaypalOrder.php
@@ -235,7 +235,7 @@ class GenerateJsonPaypalOrder
             return (string) $str;
         }
 
-        return substr($str, 0, $limit);
+        return mb_substr($str, 0, $limit);
     }
 
     /**
