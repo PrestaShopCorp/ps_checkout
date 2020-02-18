869b060311d1070e10a074d441b56422ad8b38ff
zalexki <haumalex@gmail.com>
2020-02-18 14:27:03 +0100

    fix some PR feedback and set composer to classmap authoritative

--- classes/Presenter/Transaction/TransactionPresenter.php
+++ classes/Presenter/Transaction/TransactionPresenter.php
@@ -1,28 +1,22 @@
 <?php
 /**
-* 2007-2019 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2019 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
+ * 2007-2020 PrestaShop and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
 
 namespace PrestaShop\Module\PrestashopCheckout\Presenter\Transaction;
 
@@ -75,7 +69,7 @@ class TransactionPresenter implements PresenterInterface
     private function getOrderIDByOrderReference($reference)
     {
         $sql = 'SELECT id_order FROM `' . _DB_PREFIX_ . 'orders` o
-            WHERE reference = "' . $reference . '"
+            WHERE reference = "' . pSQL($reference) . '"
         ';
 
         return \Db::getInstance()->getValue($sql);
@@ -91,7 +85,7 @@ class TransactionPresenter implements PresenterInterface
     private function getUserInfos($orderReference)
     {
         $link = new LinkAdapter();
-        $sql = 'SELECT id_customer FROM `' . _DB_PREFIX_ . 'orders` WHERE reference = "' . $orderReference . '"';
+        $sql = 'SELECT id_customer FROM `' . _DB_PREFIX_ . 'orders` WHERE reference = "' . pSQL($orderReference) . '"';
         $userID = \Db::getInstance()->getRow($sql);
 
         $sql = 'SELECT firstname,lastname FROM `' . _DB_PREFIX_ . 'customer` WHERE id_customer = ' . (int) $userID['id_customer'];
