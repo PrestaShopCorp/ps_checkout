7b240d88a8625bd0a870de77e0a023a83be5f672
Matt75 <5262628+Matt75@users.noreply.github.com>
2020-06-05 16:38:07 +0200

    Fix PHPStan feedback

--- controllers/admin/AdminAjaxPrestashopCheckoutController.php
+++ controllers/admin/AdminAjaxPrestashopCheckoutController.php
@@ -692,6 +692,7 @@ class AdminAjaxPrestashopCheckoutController extends ModuleAdminController
         $loggerDirectory = $this->module->getService('ps_checkout.logger.directory');
         /** @var LoggerFileReader $loggerFileReader */
         $loggerFileReader = $this->module->getService('ps_checkout.logger.file.reader');
+        $fileData = [];
 
         try {
             $fileData = $loggerFileReader->read(
