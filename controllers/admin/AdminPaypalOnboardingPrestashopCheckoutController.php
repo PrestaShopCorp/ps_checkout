49e4af92dc616f4281e4bfe80253cc6a74cf017c
Yohan Barilleau <barilyoh@gmail.com>
2020-08-10 10:27:57 +0200

    Add track on server paypal account fully approved

--- controllers/admin/AdminPaypalOnboardingPrestashopCheckoutController.php
+++ controllers/admin/AdminPaypalOnboardingPrestashopCheckoutController.php
@@ -41,6 +41,9 @@ class AdminPaypalOnboardingPrestashopCheckoutController extends ModuleAdminContr
 
         (new PaypalAccountUpdater($paypalAccount))->update();
 
+        // track account paypal fully approved
+        $this->module->getService('ps_checkout.segment.tracker')->track('Account Paypal Fully Approved', Shop::getContextListShopID());
+
         Tools::redirect(
             (new LinkAdapter($this->context->link))->getAdminLink(
                 'AdminModules',
