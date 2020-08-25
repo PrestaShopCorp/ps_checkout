c463eb2046c2a781705343582fb8524e9864142a
Yohan Barilleau <barilyoh@gmail.com>
2020-08-25 09:04:41 +0200

    Update upgrade method with default payment methods order values

--- upgrade/upgrade-2.0.0.php
+++ upgrade/upgrade-2.0.0.php
@@ -17,6 +17,9 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
  */
+
+use PrestaShop\Module\PrestashopCheckout\PaymentOptions\PaymentOptionsProvider;
+
 if (!defined('_PS_VERSION_')) {
     exit;
 }
@@ -30,8 +33,17 @@ if (!defined('_PS_VERSION_')) {
  */
 function upgrade_module_2_0_0($module)
 {
+    $paymentOptions = (new PaymentOptionsProvider())->createDefaultPaymentOptions();
+
     return (bool) Configuration::updateGlobalValue('PS_CHECKOUT_LOGGER_MAX_FILES', '15')
         && (bool) Configuration::updateGlobalValue('PS_CHECKOUT_LOGGER_LEVEL', \Monolog\Logger::ERROR)
         && (bool) Configuration::updateGlobalValue('PS_CHECKOUT_LOGGER_HTTP', '1')
-        && (bool) Configuration::updateGlobalValue('PS_CHECKOUT_LOGGER_HTTP_FORMAT', 'DEBUG');
+        && (bool) Configuration::updateGlobalValue('PS_CHECKOUT_LOGGER_HTTP_FORMAT', 'DEBUG')
+        && (bool) Configuration::updateValue(
+            'PS_CHECKOUT_PAYMENT_METHODS_ORDER',
+            json_encode($paymentOptions->getPaymentOptions()),
+            false,
+            null,
+            (int) \Context::getContext()->shop->id
+        );
 }
