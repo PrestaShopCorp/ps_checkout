f852d861a39db93f95a8c416b58a1f6b80f8afa9
Yohan Barilleau <barilyoh@gmail.com>
2020-09-01 15:45:30 +0200

    fix php cs fixer

--- ps_checkout.php
+++ ps_checkout.php
@@ -17,7 +17,6 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
  */
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 if (!defined('_PS_VERSION_')) {
