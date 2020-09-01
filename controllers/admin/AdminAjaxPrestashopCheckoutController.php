364432f469b94c8c7710125fcb42962ae2442a55
Yohan Barilleau <barilyoh@gmail.com>
2020-09-01 12:17:06 +0200

    fix php cs fixer

--- controllers/admin/AdminAjaxPrestashopCheckoutController.php
+++ controllers/admin/AdminAjaxPrestashopCheckoutController.php
@@ -20,7 +20,6 @@
 use Monolog\Logger;
 use PrestaShop\Module\PrestashopCheckout\Api\Payment\Onboarding;
 use PrestaShop\Module\PrestashopCheckout\Api\Psx\Onboarding as PsxOnboarding;
-use PrestaShop\Module\PrestashopCheckout\Configuration\PrestaShopConfiguration;
 use PrestaShop\Module\PrestashopCheckout\Logger\LoggerDirectory;
 use PrestaShop\Module\PrestashopCheckout\Logger\LoggerFactory;
 use PrestaShop\Module\PrestashopCheckout\Logger\LoggerFileFinder;
