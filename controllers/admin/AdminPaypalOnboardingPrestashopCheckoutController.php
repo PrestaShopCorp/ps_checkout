e2292876817f1937938828466c623d2ed6d31221
Yohan Barilleau <barilyoh@gmail.com>
2020-09-02 17:21:51 +0200

    fix php cs fixer

--- controllers/admin/AdminPaypalOnboardingPrestashopCheckoutController.php
+++ controllers/admin/AdminPaypalOnboardingPrestashopCheckoutController.php
@@ -47,8 +47,7 @@ class AdminPaypalOnboardingPrestashopCheckoutController extends ModuleAdminContr
 
         (new PaypalAccountUpdater($paypalAccount))->update();
 
-        if($paypalAccount->getCardPaymentStatus() === PaypalAccountUpdater::SUBSCRIBED)
-        {
+        if ($paypalAccount->getCardPaymentStatus() === PaypalAccountUpdater::SUBSCRIBED) {
             // track account paypal fully approved
             $this->module->getService('ps_checkout.segment.tracker')->track('Account Paypal Fully Approved', Shop::getContextListShopID());
         }
