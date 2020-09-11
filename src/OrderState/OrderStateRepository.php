7c7cb90d16c6d7a7c179ceb9cff020c53e5c1428
Yohan Barilleau <barilyoh@gmail.com>
2020-09-11 11:16:23 +0200

    update icon when update an existing orderState

--- src/OrderState/OrderStateRepository.php
+++ src/OrderState/OrderStateRepository.php
@@ -196,6 +196,10 @@ class OrderStateRepository
             throw new OrderStateException(sprintf('Failed to update OrderState %s', $orderState->getConfigurationKey()), OrderStateException::ORDER_STATE_NOT_UPDATED);
         }
 
+        if (false === (bool) \Tools::copy($orderState->getIcon(), _PS_ORDER_STATE_IMG_DIR_ . $orderStateId . '.gif')) {
+            throw new OrderStateException(sprintf('Failed to copy icon of OrderState %s', $orderState->getConfigurationKey()),OrderStateException::ORDER_STATE_ICON_NOT_COPIED);
+        }
+
         return $result;
     }
 
