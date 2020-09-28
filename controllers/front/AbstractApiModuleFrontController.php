40515d93fd215b1a196ae7d5b4563e89e211999b
Yohan Barilleau <barilyoh@gmail.com>
2020-09-28 17:02:29 +0200

    fix phpcsfixer

--- controllers/front/AbstractApiModuleFrontController.php
+++ controllers/front/AbstractApiModuleFrontController.php
@@ -22,7 +22,6 @@ use PrestaShop\Module\PrestashopCheckout\Exception\PsCheckoutException;
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
  */
-
 abstract class AbstractApiModuleFrontController extends ModuleFrontController
 {
     /**
