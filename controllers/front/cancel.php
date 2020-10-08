939ac636644c135df6804fd6151310e7877973ee
Matt75 <5262628+Matt75@users.noreply.github.com>
2020-10-08 15:08:57 +0200

    Reset isExpressCheckout on cancel

--- controllers/front/cancel.php
+++ controllers/front/cancel.php
@@ -44,23 +44,49 @@ class Ps_CheckoutCancelModuleFrontController extends ModuleFrontController
                 throw new PsCheckoutException('Bad token', PsCheckoutException::PSCHECKOUT_BAD_STATIC_TOKEN);
             }
 
+            if (false === Validate::isLoadedObject($this->context->cart)) {
+                throw new PsCheckoutException('No cart found.', PsCheckoutException::PRESTASHOP_CONTEXT_INVALID);
+            }
+
             $bodyContent = file_get_contents('php://input');
 
-            if (false === empty($bodyContent)) {
-                $bodyValues = json_decode($bodyContent, true);
-
-                if (false === empty($bodyValues['orderID'])) {
-                    $this->module->getLogger()->info(sprintf(
-                        'Customer canceled payment - PayPal Order %s',
-                        $bodyValues['orderID']
-                    ));
-                }
+            if (empty($bodyContent)) {
+                throw new PsCheckoutException('Payload invalid', PsCheckoutException::PSCHECKOUT_WEBHOOK_BODY_EMPTY);
             }
+
+            $bodyValues = json_decode($bodyContent, true);
+
+            if (empty($bodyValues)) {
+                throw new PsCheckoutException('Payload invalid', PsCheckoutException::PSCHECKOUT_WEBHOOK_BODY_EMPTY);
+            }
+
+            /** @var \PrestaShop\Module\PrestashopCheckout\Repository\PsCheckoutCartRepository $psCheckoutCartRepository */
+            $psCheckoutCartRepository = $this->module->getService('ps_checkout.repository.pscheckoutcart');
+
+            /** @var PsCheckoutCart|false $psCheckoutCart */
+            $psCheckoutCart = $psCheckoutCartRepository->findOneByCartId((int) $this->context->cart->id);
+
+            if (false !== $psCheckoutCart) {
+                $psCheckoutCart->paypal_funding = $bodyValues['fundingSource'];
+                $psCheckoutCart->isExpressCheckout = false;
+                $psCheckoutCart->isHostedFields = false;
+                $psCheckoutCartRepository->save($psCheckoutCart);
+            }
+
+            $this->module->getLogger()->info(
+                'Customer canceled payment',
+                [
+                    'PayPalOrderId' => isset($bodyValues['orderID']) ? $bodyValues['orderID'] : null,
+                    'FundingSource' => isset($bodyValues['fundingSource']) ? $bodyValues['fundingSource'] : null,
+                    'isExpressCheckout' => isset($bodyValues['isExpressCheckout']) ? (bool) $bodyValues['isExpressCheckout'] : false,
+                    'isHostedFields' => isset($bodyValues['isHostedFields']) ? (bool) $bodyValues['isHostedFields'] : false,
+                ]
+            );
 
             echo json_encode([
                 'status' => true,
                 'httpCode' => 200,
-                'body' => isset($bodyValues) ? $bodyValues : '',
+                'body' => $bodyValues,
                 'exceptionCode' => null,
                 'exceptionMessage' => null,
             ]);
