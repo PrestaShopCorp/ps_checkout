7cd74f3826632a3ec015bf1d2f0da225a253a3c7
Pedro Pérez Furio <perez-furio.ext@prestashop.com>
2020-11-04 10:44:12 +0100

    Added @babel/preset-env (accidentally removed) and Terser

--- _dev/js/front/webpack/webpack.config.js
+++ _dev/js/front/webpack/webpack.config.js
@@ -38,7 +38,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
       }
     ]
