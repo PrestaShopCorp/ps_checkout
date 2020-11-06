56fc1341d6cbf962f655f416a6f41110fc2e1c93
Pedro Pérez Furio <perez-furio.ext@prestashop.com>
2020-11-06 07:44:11 +0100

    Added static variables syntax

--- _dev/js/front/.eslintrc.js
+++ _dev/js/front/.eslintrc.js
@@ -6,11 +6,12 @@ module.exports = {
     node: true
   },
   extends: ['eslint:recommended'],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
-    parser: 'babel-eslint',
     sourceType: 'module'
   },
+  plugins: ['babel'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
