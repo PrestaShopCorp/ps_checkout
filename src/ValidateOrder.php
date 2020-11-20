b14811d2c998deed442c44960e982e77548bc6a5
Matt75 <5262628+Matt75@users.noreply.github.com>
2020-11-20 10:47:36 +0100

    Remove old funding source provider

--- src/ValidateOrder.php
+++ src/ValidateOrder.php
@@ -121,8 +121,8 @@ class ValidateOrder
             /** @var \Ps_checkout $module */
             $module = \Module::getInstanceByName('ps_checkout');
 
-            /** @var \PrestaShop\Module\PrestashopCheckout\FundingSourceProvider $fundingSourceProvider */
-            $fundingSourceProvider = $module->getService('ps_checkout.provider.funding_source');
+            /** @var \PrestaShop\Module\PrestashopCheckout\FundingSource\FundingSourceTranslationProvider $fundingSourceTranslationProvider */
+            $fundingSourceTranslationProvider = $module->getService('ps_checkout.funding_source.translation');
 
             /** @var \PrestaShop\Module\PrestashopCheckout\Repository\PsCheckoutCartRepository $psCheckoutCartRepository */
             $psCheckoutCartRepository = $module->getService('ps_checkout.repository.pscheckoutcart');
@@ -153,7 +153,7 @@ class ValidateOrder
                     $payload['cartId'],
                     (int) $this->getOrderState($psCheckoutCart->paypal_funding),
                     $payload['amount'],
-                    $fundingSourceProvider->getPaymentMethodName($psCheckoutCart->paypal_funding),
+                    $fundingSourceTranslationProvider->getPaymentMethodName($psCheckoutCart->paypal_funding),
                     null,
                     [
                         'transaction_id' => $transactionIdentifier,
@@ -207,7 +207,7 @@ class ValidateOrder
 
                         if (false !== $orderPayment) {
                             $orderPayment->transaction_id = $transactionIdentifier;
-                            $orderPayment->payment_method = $fundingSourceProvider->getPaymentMethodName($psCheckoutCart->paypal_funding);
+                            $orderPayment->payment_method = $fundingSourceTranslationProvider->getPaymentMethodName($psCheckoutCart->paypal_funding);
                             try {
                                 $orderPayment->save();
                             } catch (\Exception $exception) {
