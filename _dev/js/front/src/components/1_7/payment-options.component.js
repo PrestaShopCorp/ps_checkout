11a6d8cc94a60f695921eec2ca6f1235ae866b90
Laurynas <laurynas.sedys@invertus.eu>
2021-04-02 13:13:00 +0300

    Removed unneeded console log

--- _dev/js/front/src/components/1_7/payment-options.component.js
+++ _dev/js/front/src/components/1_7/payment-options.component.js
@@ -41,8 +41,6 @@ export class PaymentOptionsComponent extends BaseComponent {
   }
 
   renderPaymentOptionItems() {
-    console.log('ELIGIBLE SOURCES', this.payPalService
-      .getEligibleFundingSources());
     this.children.paymentOptions = this.payPalService
       .getEligibleFundingSources()
       .map((fundingSource) => {
