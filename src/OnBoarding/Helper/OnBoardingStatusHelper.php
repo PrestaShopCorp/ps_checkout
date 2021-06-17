80efec911d0ed2fc78fbda138022fab4f8ae6890
Laurynas <laurynas.sedys@invertus.eu>
2021-06-17 17:17:42 +0300

    Fixed circular dependency

--- src/OnBoarding/Helper/OnBoardingStatusHelper.php
+++ src/OnBoarding/Helper/OnBoardingStatusHelper.php
@@ -4,12 +4,13 @@ namespace PrestaShop\Module\PrestashopCheckout\OnBoarding\Helper;
 
 use PrestaShop\Module\PrestashopCheckout\Api\Firebase\Token;
 use PrestaShop\Module\PrestashopCheckout\Configuration\PrestaShopConfiguration;
+use PrestaShop\Module\PrestashopCheckout\Entity\PsAccount;
 use PrestaShop\Module\PsAccounts\Service\PsAccountsService;
 use PrestaShop\PsAccountsInstaller\Installer\Facade\PsAccounts;
 use PrestaShop\Module\PrestashopCheckout\Presenter\PresenterInterface;
 
 
-class OnBoardingStatusHelper implements PresenterInterface
+class OnBoardingStatusHelper
 {
     /** @var PrestaShopConfiguration */
     private $configuration;
@@ -27,11 +28,19 @@ class OnBoardingStatusHelper implements PresenterInterface
         $this->psAccountsFacade = $psAccountsFacade;
     }
 
+    /**
+     * @return bool
+     *
+     * Did not use (new Token)->getToken() because it would create a circular dependency
+     */
     public function isPsCheckoutOnboarded()
     {
-        return !empty((new Token())->getToken());
+        return !empty($this->configuration->get(PsAccount::PS_ACCOUNTS_FIREBASE_ID_TOKEN));
     }
 
+    /**
+     * @return false
+     */
     public function isPsAccountsOnboarded()
     {
         try {
@@ -40,15 +49,5 @@ class OnBoardingStatusHelper implements PresenterInterface
         } catch (\Exception $exception) {
             return false;
         }
-    }
-
-    public function present()
-    {
-        return [
-            'onboarding' => [
-                'isPsAccountsOnboarded' => $this->isPsAccountsOnboarded(),
-                'isPsCheckoutOnboarded' => $this->isPsCheckoutOnboarded(),
-            ]
-        ];
     }
 }
