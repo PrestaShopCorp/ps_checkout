6f250981abca9e3debeb40e18329d3cb7b5450cb
Matt75 <5262628+Matt75@users.noreply.github.com>
2022-08-08 09:16:23 +0200

    Fix error on custom 1.7 theme if price cannot be found

--- _dev/js/front/src/service/prestashop.service/prestashop-ps1_7.service.js
+++ _dev/js/front/src/service/prestashop.service/prestashop-ps1_7.service.js
@@ -86,14 +86,18 @@ export class PrestashopPs1_7Service {
       productPrice = document.querySelector('.current-price [itemprop="price"]');
     }
 
-    return productPrice.getAttribute('content') || '';
+    if (productPrice) {
+      return productPrice.getAttribute('content');
+    }
+
+    return '';
   }
 
   static isAddToCartButtonDisabled() {
     let addToCartElement = document.querySelector('.page-product:not(.modal-open) .row .product-add-to-cart, .page-product:not(.modal-open) .product-container .product-add-to-cart, .page-product:not(.modal-open) .row .js-product-add-to-cart, .page-product:not(.modal-open) .product-container .js-product-add-to-cart');
     let addToCartButtonElement = addToCartElement.querySelector('button.add-to-cart');
 
-    return addToCartButtonElement.disabled;
+    return addToCartButtonElement ? addToCartButtonElement.disabled : true;
   }
 
   static onUpdatedCart(listener) {
