1f4e19e74ae7ad390ad3614888ff77833cf91228
Matt75 <5262628+Matt75@users.noreply.github.com>
2022-09-29 17:37:50 +0200

    Bump module version

--- ps_checkout.php
+++ ps_checkout.php
@@ -128,7 +128,7 @@ class Ps_checkout extends PaymentModule
 
     // Needed in order to retrieve the module version easier (in api call headers) than instanciate
     // the module each time to get the version
-    const VERSION = '2.20.2';
+    const VERSION = '3.0.0';
 
     const INTEGRATION_DATE = '2022-14-06';
 
@@ -154,7 +154,7 @@ class Ps_checkout extends PaymentModule
 
         // We cannot use the const VERSION because the const is not computed by addons marketplace
         // when the zip is uploaded
-        $this->version = '2.20.2';
+        $this->version = '3.0.0';
         $this->author = 'PrestaShop';
         $this->currencies = true;
         $this->currencies_mode = 'checkbox';
