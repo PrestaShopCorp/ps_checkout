fb977a297d956709028607e7882fdcc504b30763
Matt75 <5262628+Matt75@users.noreply.github.com>
2023-01-12 17:51:33 +0100

    Fix BO Order page when legacy parameter is used

--- ps_checkout.php
+++ ps_checkout.php
@@ -846,7 +846,7 @@ class Ps_checkout extends PaymentModule
             );
         }
 
-        if ('AdminOrders' === Tools::getValue('controller')) {
+        if ('AdminOrders' === Tools::getValue('controller') || 'AdminOrders' === Tools::getValue('tab')) {
             $this->context->controller->addJS(
                 $this->getPathUri() . 'views/js/adminOrderView.js?version=' . $this->version,
                 false
