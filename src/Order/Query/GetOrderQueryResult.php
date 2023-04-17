63573a270ea636f2e6341a484e6e39f395999c46
Bastien Tafforeau <bastien.tafforeau@gmail.com>
2023-04-17 14:19:26 +0200

    Adding GetOrderQuery

---
+++ src/Order/Query/GetOrderQueryResult.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace PrestaShop\Module\PrestashopCheckout\Order\Query;
+
+class GetOrderQueryResult
+{
+    /** @var int */
+    private $id;
+
+    /** @var int */
+    private $currentState;
+
+    /** @var bool */
+    private $hasBeenPaid;
+
+    /** @var bool */
+    private $hasBeenShipped;
+
+    /** @var bool */
+    private $hasBeenDelivered;
+
+    /** @var bool */
+    private $isInPreparation;
+
+    /** @var string */
+    private $totalAmount;
+
+    /** @var string */
+    private $totalAmountPaid;
+
+    /**
+     * @param int $id
+     * @param int $currentState
+     * @param bool $hasBeenPaid
+     * @param bool $hasBeenShipped
+     * @param bool $hasBeenDelivered
+     * @param bool $isInPreparation
+     * @param string $totalAmount
+     * @param string $totalAmountPaid
+     */
+    public function __construct(
+        $id,
+        $currentState,
+        $hasBeenPaid,
+        $hasBeenShipped,
+        $hasBeenDelivered,
+        $isInPreparation,
+        $totalAmount,
+        $totalAmountPaid
+    )
+    {
+        $this->id = $id;
+        $this->currentState = $currentState;
+        $this->hasBeenPaid = $hasBeenPaid;
+        $this->hasBeenShipped = $hasBeenShipped;
+        $this->hasBeenDelivered = $hasBeenDelivered;
+        $this->isInPreparation = $isInPreparation;
+        $this->totalAmount = $totalAmount;
+        $this->totalAmountPaid = $totalAmountPaid;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCurrentState()
+    {
+        return $this->currentState;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasBeenPaid()
+    {
+        return $this->hasBeenPaid;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasBeenShipped()
+    {
+        return $this->hasBeenShipped;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasBeenDelivered()
+    {
+        return $this->hasBeenDelivered;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isInPreparation()
+    {
+        return $this->isInPreparation;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTotalAmount()
+    {
+        return $this->totalAmount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTotalAmountPaid()
+    {
+        return $this->totalAmountPaid;
+    }
+}
