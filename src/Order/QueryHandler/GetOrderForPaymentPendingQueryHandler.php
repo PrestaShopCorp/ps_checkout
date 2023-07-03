f72f7c79d3d9994ab33b46492cae6cc8a7e0d986
sgodard <sebastien.godard@prestashop?com>
2023-07-03 11:08:36 +0200

    Remove install

--- src/Order/QueryHandler/GetOrderForPaymentPendingQueryHandler.php
+++ src/Order/QueryHandler/GetOrderForPaymentPendingQueryHandler.php
@@ -96,7 +96,7 @@ class GetOrderForPaymentPendingQueryHandler
     private function isInPending(Order $order)
     {
         return count($order->getHistory($order->id_lang, (int) Configuration::getGlobalValue(OrderStateConfigurationKeys::WAITING_PAYMENT)))
-            ||count($order->getHistory($order->id_lang, (int) Configuration::getGlobalValue(OrderStateConfigurationKeys::WAITING_CREDIT_CARD_PAYMENT)))
+            || count($order->getHistory($order->id_lang, (int) Configuration::getGlobalValue(OrderStateConfigurationKeys::WAITING_CREDIT_CARD_PAYMENT)))
             || count($order->getHistory($order->id_lang, (int) Configuration::getGlobalValue(OrderStateConfigurationKeys::WAITING_PAYPAL_PAYMENT)))
             || count($order->getHistory($order->id_lang, (int) Configuration::getGlobalValue(OrderStateConfigurationKeys::WAITING_LOCAL_PAYMENT)));
     }
--- src/Translations/OrderStatesTranslations.php
+++ src/Translations/OrderStatesTranslations.php
@@ -31,7 +31,7 @@ class OrderStatesTranslations
         'nl' => 'Wachten op betaling',
         'de' => 'Warten auf Zahlung',
         'pl' => 'Oczekiwanie na płatność',
-        'pt' => 'Aguardando pagamento'
+        'pt' => 'Aguardando pagamento',
     ];
     const PS_CHECKOUT_STATE_AUTHORIZED = [
         'en' => 'Authorized. To be captured by merchant',
@@ -75,9 +75,6 @@ class OrderStatesTranslations
 
         return [
             'PS_CHECKOUT_STATE_WAITING_PAYMENT' => self::PS_CHECKOUT_STATE_WAITING_PAYMENT[$isoCode],
-            'PS_CHECKOUT_STATE_WAITING_PAYPAL_PAYMENT' => self::PS_CHECKOUT_STATE_WAITING_PAYPAL_PAYMENT[$isoCode],
-            'PS_CHECKOUT_STATE_WAITING_CREDIT_CARD_PAYMENT' => self::PS_CHECKOUT_STATE_WAITING_CREDIT_CARD_PAYMENT[$isoCode],
-            'PS_CHECKOUT_STATE_WAITING_LOCAL_PAYMENT' => self::PS_CHECKOUT_STATE_WAITING_LOCAL_PAYMENT[$isoCode],
             'PS_CHECKOUT_STATE_AUTHORIZED' => self::PS_CHECKOUT_STATE_AUTHORIZED[$isoCode],
             'PS_CHECKOUT_STATE_PARTIAL_REFUND' => self::PS_CHECKOUT_STATE_PARTIAL_REFUND[$isoCode],
             'PS_CHECKOUT_STATE_WAITING_CAPTURE' => self::PS_CHECKOUT_STATE_WAITING_CAPTURE[$isoCode],
@@ -94,9 +91,6 @@ class OrderStatesTranslations
     private function confirmIsoCode($isoCode)
     {
         if (!array_key_exists($isoCode, self::PS_CHECKOUT_STATE_WAITING_PAYMENT) ||
-            !array_key_exists($isoCode, self::PS_CHECKOUT_STATE_WAITING_PAYPAL_PAYMENT) ||
-            !array_key_exists($isoCode, self::PS_CHECKOUT_STATE_WAITING_CREDIT_CARD_PAYMENT) ||
-            !array_key_exists($isoCode, self::PS_CHECKOUT_STATE_WAITING_LOCAL_PAYMENT) ||
             !array_key_exists($isoCode, self::PS_CHECKOUT_STATE_AUTHORIZED) ||
             !array_key_exists($isoCode, self::PS_CHECKOUT_STATE_PARTIAL_REFUND) ||
             !array_key_exists($isoCode, self::PS_CHECKOUT_STATE_WAITING_CAPTURE)) {
