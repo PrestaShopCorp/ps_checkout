1667c483c283fae4d20f9cc56faae2354e3cef91
Matt75 <5262628+Matt75@users.noreply.github.com>
2023-07-13 15:52:20 +0200

    Update upgrade files

--- upgrade/upgrade-6.3.4.0.php
+++ upgrade/upgrade-6.3.4.0.php
@@ -30,32 +30,186 @@ if (!defined('_PS_VERSION_')) {
  */
 function upgrade_module_6_3_4_0($module)
 {
-    $orderStateCollection = new PrestaShopCollection(OrderState::class);
-    $orderStateCollection->where('module_name', '=', $module->name);
-    $orderStateCollection->where('deleted', '=', '0');
+    // Force PrestaShop to upgrade for all shop to avoid issues
+    $savedShopContext = Shop::getContext();
+    $savedShopId = Shop::getContextShopID();
+    $savedGroupShopId = Shop::getContextShopGroupID();
+    Shop::setContext(Shop::CONTEXT_ALL);
 
-    /** @var OrderState[] $orderStates */
-    $orderStates = $orderStateCollection->getResults();
+    try {
+        $shopsList = Shop::getShops(false, null, true);
 
-    if (!empty($orderStates)) {
-        foreach ($orderStates as $orderState) {
-            if (in_array($orderState->id, [Configuration::getGlobalValue('PS_CHECKOUT_STATE_WAITING_PAYPAL_PAYMENT'), Configuration::getGlobalValue('PS_CHECKOUT_STATE_WAITING_CREDIT_CARD_PAYMENT'), Configuration::getGlobalValue('PS_CHECKOUT_STATE_WAITING_LOCAL_PAYMENT')])) {
-                $orderState->deleted = true;
-                $orderState->save();
+        foreach ($shopsList as $shopId) {
+            Configuration::updateValue('PS_CHECKOUT_LIABILITY_SHIFT_REQ', '0', false, null, (int) $shopId);
+        }
+
+        Configuration::updateGlobalValue('PS_CHECKOUT_LIABILITY_SHIFT_REQ', '0');
+
+        $db = Db::getInstance();
+
+        // Installing FundingSource if table pscheckout_funding_source is empty or incomplete - including BLIK
+        $fundingSources = ['paypal', 'paylater', 'card', 'bancontact', 'eps', 'giropay', 'ideal', 'mybank', 'p24', 'sofort', 'blik'];
+        $availableFundingSourcesByShops = [];
+        $maxPositionByShops = [];
+        $availableFundingSources = $db->executeS('SELECT * FROM ' . _DB_PREFIX_ . 'pscheckout_funding_source');
+
+        if (!empty($availableFundingSources)) {
+            foreach ($availableFundingSources as $availableFundingSource) {
+                $currentPosition = (int) $availableFundingSource['position'];
+                $shopId = (int) $availableFundingSource['id_shop'];
+
+                if (
+                    !isset($maxPositionByShops[$shopId])
+                    || $maxPositionByShops[$shopId] < $currentPosition
+                ) {
+                    $maxPositionByShops[$shopId] = $currentPosition;
+                }
+
+                $availableFundingSourcesByShops[$shopId][] = $availableFundingSource['name'];
             }
         }
+
+        foreach (Shop::getShops(false, null, true) as $shopId) {
+            $currentPosition = isset($maxPositionByShops[(int) $shopId]) ? $maxPositionByShops[(int) $shopId] + 1 : 1;
+            foreach ($fundingSources as $fundingSource) {
+                if (
+                    !isset($availableFundingSourcesByShops[(int) $shopId])
+                    || !in_array($fundingSource, $availableFundingSourcesByShops[(int) $shopId], true)
+                ) {
+                    $db->insert(
+                        'pscheckout_funding_source',
+                        [
+                            'name' => pSQL($fundingSource),
+                            'active' => 1,
+                            'position' => (int) $currentPosition,
+                            'id_shop' => (int) $savedShopId,
+                        ]
+                    );
+                    ++$currentPosition;
+                }
+            }
+        }
+
+        // Check module OrderState
+        $moduleOrderStates = [
+            'PS_CHECKOUT_STATE_AUTHORIZED' => (int) Configuration::getGlobalValue('PS_CHECKOUT_STATE_AUTHORIZED'),
+            'PS_CHECKOUT_STATE_PARTIAL_REFUND' => (int) Configuration::getGlobalValue('PS_CHECKOUT_STATE_PARTIAL_REFUND'),
+            'PS_CHECKOUT_STATE_WAITING_CAPTURE' => (int) Configuration::getGlobalValue('PS_CHECKOUT_STATE_WAITING_CAPTURE'),
+            'PS_CHECKOUT_STATE_WAITING_PAYMENT' => (int) Configuration::getGlobalValue('PS_CHECKOUT_STATE_WAITING_PAYMENT'),
+        ];
+        $moduleOrderStatesId = array_values($moduleOrderStates);
+        $moduleOrderStatesIdToDelete = [
+            (int) Configuration::getGlobalValue('PS_CHECKOUT_STATE_WAITING_PAYPAL_PAYMENT'),
+            (int) Configuration::getGlobalValue('PS_CHECKOUT_STATE_WAITING_CREDIT_CARD_PAYMENT'),
+            (int) Configuration::getGlobalValue('PS_CHECKOUT_STATE_WAITING_LOCAL_PAYMENT'),
+        ];
+
+        $orderStateCollection = new PrestaShopCollection(OrderState::class);
+        $orderStateCollection->where('module_name', '=', $module->name);
+        $orderStateCollection->where('deleted', '=', '0');
+
+        /** @var OrderState[] $orderStates */
+        $orderStates = $orderStateCollection->getResults();
+        $currentModuleOrderStatesId = [];
+
+        if (!empty($orderStates)) {
+            foreach ($orderStates as $orderState) {
+                $orderStateId = (int) $orderState->id;
+                if (
+                    !in_array($orderStateId, $moduleOrderStatesId, true)
+                    || in_array($orderState->id, $moduleOrderStatesIdToDelete, true)
+                ) {
+                    $orderState->deleted = true;
+                    $orderState->save();
+                } else {
+                    $currentModuleOrderStatesId[] = $orderStateId;
+                }
+            }
+        }
+
+        foreach ($moduleOrderStates as $configuration_key => $id_order_state) {
+            if (
+                !$id_order_state
+                || !in_array((int) $id_order_state, $currentModuleOrderStatesId, true)
+            ) {
+                switch ($configuration_key) {
+                    case 'PS_CHECKOUT_STATE_AUTHORIZED':
+                        ps_checkout_create_order_state_6_3_4_0(
+                            'PS_CHECKOUT_STATE_AUTHORIZED',
+                            '#3498D8',
+                            [
+                                'en' => 'Authorized. To be captured by merchant',
+                                'fr' => 'Autorisation. A capturer par le marchand',
+                                'es' => 'Autorizado. El vendedor lo capturará',
+                                'it' => 'Autorizzato. Sarà acquisito dal commerciante',
+                                'nl' => 'Goedgekeurd. Door retailer te registreren.',
+                                'de' => 'Autorisiert. Wird von Händler erfasst.',
+                                'pl' => 'Pomyślna autoryzacja. Transfer do przeprowadzenia przez sklep',
+                                'pt' => 'Autorizado. A ser capturado pelo comerciante',
+                            ]
+                        );
+                        break;
+                    case 'PS_CHECKOUT_STATE_PARTIAL_REFUND':
+                        ps_checkout_create_order_state_6_3_4_0(
+                            'PS_CHECKOUT_STATE_PARTIAL_REFUND',
+                            '#01B887',
+                            [
+                                'en' => 'Partial refund',
+                                'fr' => 'Remboursement partiel',
+                                'es' => 'Reembolso parcial',
+                                'it' => 'Rimborso parziale',
+                                'nl' => 'Gedeeltelijke terugbetaling',
+                                'de' => 'Teilweise Rückerstattung',
+                                'pl' => 'Częściowy zwrot',
+                                'pt' => 'Reembolso parcial',
+                            ]
+                        );
+                        break;
+                    case 'PS_CHECKOUT_STATE_WAITING_PAYMENT':
+                        ps_checkout_create_order_state_6_3_4_0('PS_CHECKOUT_STATE_WAITING_PAYMENT', '#34209E', [
+                            'en' => 'Waiting for payment',
+                            'fr' => 'En attente de paiement',
+                            'es' => 'Esperando el pago',
+                            'it' => 'In attesa di pagamento',
+                            'nl' => 'Wachten op betaling',
+                            'de' => 'Warten auf Zahlung',
+                            'pl' => 'Oczekiwanie na płatność',
+                            'pt' => 'Aguardando pagamento',
+                        ]);
+                        break;
+                    case 'PS_CHECKOUT_STATE_WAITING_CAPTURE':
+                        ps_checkout_create_order_state_6_3_4_0(
+                            'PS_CHECKOUT_STATE_WAITING_CAPTURE',
+                            '#3498D8',
+                            [
+                                'en' => 'Waiting capture',
+                                'fr' => 'En attente de capture',
+                                'es' => 'Esperando la captura',
+                                'it' => 'In attesa di essere acquisito',
+                                'nl' => 'Wachten op registratie',
+                                'de' => 'Warten auf Erfassung',
+                                'pl' => 'Oczekiwanie na transfer',
+                                'pt' => 'Aguardando a captura',
+                            ]
+                        );
+                        break;
+                }
+            }
+        }
+    } catch (Exception $exception) {
+        PrestaShopLogger::addLog($exception->getMessage(), 3, $exception->getCode(), 'Module', $module->id);
+
+        return false;
     }
 
-    ps_checkout_create_order_state_6_3_4_0('PS_CHECKOUT_STATE_WAITING_PAYMENT', '#34209E', [
-        'en' => 'Waiting for payment',
-        'fr' => 'En attente de paiement',
-        'es' => 'Esperando el pago',
-        'it' => 'In attesa di pagamento',
-        'nl' => 'Wachten op betaling',
-        'de' => 'Warten auf Zahlung',
-        'pl' => 'Oczekiwanie na płatność',
-        'pt' => 'Aguardando pagamento',
-    ]);
+    // Restore initial PrestaShop shop context
+    if (Shop::CONTEXT_SHOP === $savedShopContext) {
+        Shop::setContext($savedShopContext, $savedShopId);
+    } elseif (Shop::CONTEXT_GROUP === $savedShopContext) {
+        Shop::setContext($savedShopContext, $savedGroupShopId);
+    } else {
+        Shop::setContext($savedShopContext);
+    }
 
     return true;
 }
