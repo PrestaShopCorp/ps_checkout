7e9addb5faa36bfb2449a438541befeb0f7f24bc
sgodard <sebastien.godard@prestashop.com>
2023-12-08 23:29:20 +0100

    add test for P24

--- tests/Unit/PaymentSource/P24PaymentSourceTest.php
+++ tests/Unit/PaymentSource/P24PaymentSourceTest.php
@@ -1,8 +1,12 @@
 <?php
 
-namespace Tests\Unit\Amount;
+namespace Tests\Unit\PaymentSource;
 
 use PHPUnit\Framework\TestCase;
+use PrestaShop\Module\PrestashopCheckout\Country\Exception\CountryException;
+use PrestaShop\Module\PrestashopCheckout\Currency\Exception\CurrencyException;
+use PrestaShop\Module\PrestashopCheckout\Intent\Exception\IntentException;
+use PrestaShop\Module\PrestashopCheckout\Intent\ValueObject\Intent;
 use PrestaShop\Module\PrestashopCheckout\PaymentSource\EligibilityRule\AmountEligibilityRule;
 use PrestaShop\Module\PrestashopCheckout\PaymentSource\EligibilityRule\CountryEligibilityRule;
 use PrestaShop\Module\PrestashopCheckout\PaymentSource\EligibilityRule\CurrencyEligibilityRule;
@@ -10,6 +14,7 @@ use PrestaShop\Module\PrestashopCheckout\PaymentSource\EligibilityRule\IntentEli
 use PrestaShop\Module\PrestashopCheckout\PaymentSource\EligibilityRule\PageTypeEligibilityRule;
 use PrestaShop\Module\PrestashopCheckout\PaymentSource\PaymentSource;
 use PrestaShop\Module\PrestashopCheckout\PaymentSource\PaymentSourceUseCase;
+use PrestaShop\Module\PrestashopCheckout\PaymentSourceUseCase\Exception\PaymentSourceUseCaseException;
 use PrestaShop\Module\PrestashopCheckout\Rule\AndRule;
 use PrestaShop\Module\PrestashopCheckout\Rule\NotRule;
 use PrestaShop\Module\PrestashopCheckout\Rule\OrRule;
@@ -18,8 +23,13 @@ class P24PaymentSourceTest extends TestCase
 {
     /**
      * @dataProvider invalidP24DataProvider
+     *
+     * @throws CurrencyException
+     * @throws CountryException
+     * @throws IntentException
+     * @throws PaymentSourceUseCaseException
      */
-    public function testInvalidP24PaymentSource($data)
+    public function testInvalidP24PaymentSource($data, $paymentSourceRulesExpected, $paymentSourceUseCaseExpected)
     {
         $paymentSource = new PaymentSource(
             'p24',
@@ -49,12 +59,38 @@ class P24PaymentSourceTest extends TestCase
                 new PaymentSourceUseCase(
                     'ECM',
                     [
-                        new IntentEligibilityRule($data['intent'], ['CAPTURE']),
+                        new IntentEligibilityRule(new Intent($data['intent']), ['CAPTURE']),
                         new PageTypeEligibilityRule($data['pageType'], ['checkout']),
                     ]
                 ),
             ]
         );
+        $this->rulesTesting($paymentSource->getRules(), $paymentSourceRulesExpected);
+        $this->UseCasesTesting($paymentSource->getUseCases(), $paymentSourceUseCaseExpected);
+    }
+
+    private function rulesTesting($rules, $resultExpected)
+    {
+        foreach ($rules as $rule) {
+            $this->assertEquals($rule->evaluate(), $resultExpected[get_class($rule)]);
+        }
+    }
+
+    private function UseCasesTesting($useCases, $resultExpected)
+    {
+        foreach ($useCases as $useCase) {
+            $isAvailableUseCase = true;
+            foreach ($useCase->getRules() as $rule) {
+                $isAvailableUseCase = $rule->evaluate();
+                if (!$isAvailableUseCase) {
+                    $this->assertFalse(in_array($useCase->getType(), $resultExpected));
+                    break;
+                }
+            }
+            if ($isAvailableUseCase) {
+                $this->assertTrue(in_array($useCase->getType(), $resultExpected));
+            }
+        }
     }
 
     public function invalidP24DataProvider()
@@ -69,6 +105,14 @@ class P24PaymentSourceTest extends TestCase
                     'merchantCountry' => 'US',
                     'pageType' => 'checkout',
                 ],
+                [
+                    OrRule::class => false,
+                    CountryEligibilityRule::class => true,
+                    NotRule::class => true,
+                ],
+                [
+                    'ECM',
+                ],
             ],
             [
                 [
@@ -79,45 +123,83 @@ class P24PaymentSourceTest extends TestCase
                     'merchantCountry' => 'US',
                     'pageType' => 'checkout',
                 ],
+                [
+                    OrRule::class => true,
+                    CountryEligibilityRule::class => false,
+                    NotRule::class => true,
+                ],
+                [
+                    'ECM',
+                ],
             ],
             [
                 [
                     'amount' => '9.90',
-                    'buyerCountry' => 'IT',
+                    'buyerCountry' => 'PL',
                     'currency' => 'USD', // Invalid currency
                     'intent' => 'CAPTURE',
                     'merchantCountry' => 'US',
                     'pageType' => 'checkout',
                 ],
+                [
+                    OrRule::class => false,
+                    CountryEligibilityRule::class => true,
+                    NotRule::class => true,
+                ],
+                [
+                    'ECM',
+                ],
             ],
             [
                 [
                     'amount' => '39.99',
-                    'buyerCountry' => 'IT',
+                    'buyerCountry' => 'PL',
                     'currency' => 'EUR',
                     'intent' => 'AUTHORIZE', // Invalid intent
                     'merchantCountry' => 'FR',
                     'pageType' => 'checkout',
                 ],
+                [
+                    OrRule::class => true,
+                    CountryEligibilityRule::class => true,
+                    NotRule::class => true,
+                ],
+                [
+                ],
             ],
             [
                 [
                     'amount' => '15',
-                    'buyerCountry' => 'IT',
+                    'buyerCountry' => 'PL',
                     'currency' => 'EUR',
                     'intent' => 'CAPTURE',
                     'merchantCountry' => 'JP', // Invalid merchant country
                     'pageType' => 'checkout',
                 ],
+                [
+                    OrRule::class => true,
+                    CountryEligibilityRule::class => true,
+                    NotRule::class => false,
+                ],
+                [
+                    'ECM',
+                ],
             ],
             [
                 [
                     'amount' => '39.99',
-                    'buyerCountry' => 'IT',
+                    'buyerCountry' => 'PL',
                     'currency' => 'EUR',
                     'intent' => 'CAPTURE',
                     'merchantCountry' => 'FR',
                     'pageType' => 'product', // Invalid pageType
+                ],
+                [
+                    OrRule::class => true,
+                    CountryEligibilityRule::class => true,
+                    NotRule::class => true,
+                ],
+                [
                 ],
             ],
         ];
