4a006e10890c4e5bf86c252e4651a236d623c6d3
Laurynas Šedys <laurynas.sedys.96@gmail.com>
2024-03-05 17:20:26 +0200

    [PAYSHIP-2679] New DB structure for PayPal Order (#1203)
    * Added new db structure for orders
    
    * Added entities for order
    
    * Added PayPal Order repository
    
    * Added create and delete functions
    
    * Added sql escaping
    
    * Added order saving to DB to event handler
    
    * Added new fields for order, capture and refund
    
    * phpstan fix

--- src/Database/TableManager.php
+++ src/Database/TableManager.php
@@ -94,6 +94,59 @@ class TableManager
             `data` text NOT NULL,
             PRIMARY KEY (`id_customer`, `paypal_customer_id`)
             ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;
+        ') && $this->db->execute('
+            CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'pscheckout_order` (
+            `id` varchar(50) NOT NULL,
+            `id_cart` varchar(50) NOT NULL,
+            `status` varchar(50) NOT NULL,
+            `final_capture` tinyint(1) NOT NULL,,
+            `payment_source` text,
+            `environment` varchar(50) NOT NULL,
+            `is_card_fields` tinyint(1) NOT NULL,
+            `is_express_checkout` tinyint(1) NOT NULL,
+            PRIMARY KEY (`id`)
+            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;
+        ') && $this->db->execute('
+            CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'pscheckout_capture` (
+            `id` varchar(50) NOT NULL,
+            `id_order` varchar(50) NOT NULL,
+            `status` varchar(50) NOT NULL,
+            `final_capture` tinyint(1) NOT NULL,
+            `created_at` varchar(50) NOT NULL,
+            `updated_at` varchar(50) NOT NULL,
+            `seller_protection` varchar(50) NOT NULL,
+            `seller_receivable_breakdown` text,
+            PRIMARY KEY (`id`)
+            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;
+        ') && $this->db->execute('
+            CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'pscheckout_refund` (
+            `id` varchar(50) NOT NULL,
+            `id_order` varchar(50) NOT NULL,
+            `status` varchar(50) NOT NULL,
+            `invoice_id` varchar(50) NOT NULL,
+            `custom_id` varchar(50) NOT NULL,
+            `acquirer_reference_number` varchar(50) NOT NULL,
+            `seller_payable_breakdown` text,
+            `id_order_slip` INT(10) UNSIGNED,
+            PRIMARY KEY (`id`)
+            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;
+        ') && $this->db->execute('
+            CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'pscheckout_authorization` (
+            `id` varchar(50) NOT NULL,
+            `id_order` varchar(50) NOT NULL,
+            `status` varchar(50) NOT NULL,
+            `expiration_time` varchar(50) NOT NULL,
+            `seller_protection` varchar(50) NOT NULL,
+            PRIMARY KEY (`id`)
+            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;
+        ') && $this->db->execute('
+            CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'pscheckout_purchase_unit` (
+            `id_order` varchar(50) NOT NULL,
+            `checksum` varchar(50) NOT NULL,
+            `reference_id` varchar(50) NOT NULL,
+            `items` text,
+            PRIMARY KEY (`reference_id`, `id_order`)
+            ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;
         ');
 
         $this->checkTable();
