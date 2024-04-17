28a69f437d48cfc747b5fd95c27a51dcd32c8839
Matt75 <5262628+Matt75@users.noreply.github.com>
2024-04-17 14:25:16 +0200

    Fix duplicate column name

--- src/Database/TableManager.php
+++ src/Database/TableManager.php
@@ -92,7 +92,6 @@ class TableManager
             `is_card_fields` tinyint(1) NOT NULL,
             `is_express_checkout` tinyint(1) NOT NULL,
             `customer_intent` varchar(50),
-            `customer_intent` varchar(50),
             `payment_token_id` varchar(50),
             PRIMARY KEY (`id`)
             ) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;
