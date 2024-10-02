0bd43377f35b95e4086db79934dfd4f6baf62031
Matt75 <5262628+Matt75@users.noreply.github.com>
2024-10-02 16:45:56 +0200

    Bump module version to 8.4.2.1

--- ps_checkout.php
+++ ps_checkout.php
@@ -46,7 +46,7 @@ class Ps_checkout extends PaymentModule
         'actionObjectOrderPaymentUpdateAfter',
         'displayPaymentReturn',
         'displayOrderDetail',
-        'moduleRoutes'
+        'moduleRoutes',
     ];
 
     /**
@@ -116,7 +116,7 @@ class Ps_checkout extends PaymentModule
 
     // Needed in order to retrieve the module version easier (in api call headers) than instanciate
     // the module each time to get the version
-    const VERSION = '8.4.2.0';
+    const VERSION = '8.4.2.1';
 
     const INTEGRATION_DATE = '2024-04-01';
 
@@ -137,7 +137,7 @@ class Ps_checkout extends PaymentModule
 
         // We cannot use the const VERSION because the const is not computed by addons marketplace
         // when the zip is uploaded
-        $this->version = '8.4.2.0';
+        $this->version = '8.4.2.1';
         $this->author = 'PrestaShop';
         $this->currencies = true;
         $this->currencies_mode = 'checkbox';
@@ -1803,9 +1803,9 @@ class Ps_checkout extends PaymentModule
                 'params' => [
                     'fc' => 'module',
                     'module' => 'ps_checkout',
-                    'action' => 'getDomainAssociation'
+                    'action' => 'getDomainAssociation',
                 ],
-            ]
+            ],
         ];
     }
 }
