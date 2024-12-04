138a19fc5021700c477037d0717fc77ba8d6e601
L3RAZ <laurynas.sedys@invertus.eu>
2024-12-04 17:00:13 +0200

    Added locale and error message translations

--- ps_checkout.php
+++ ps_checkout.php
@@ -1018,6 +1018,7 @@ class Ps_checkout extends PaymentModule
 
         Media::addJsDef([
             $this->name . 'Version' => $version->getSemVersion(),
+            $this->name . 'Locale' => $this->context->language->iso_code,
             $this->name . 'AutoRenderDisabled' => (bool) Configuration::get('PS_CHECKOUT_AUTO_RENDER_DISABLED'),
             $this->name . 'LoaderImage' => $this->getPathUri() . 'views/img/loader.svg',
             $this->name . 'PayPalButtonConfiguration' => $payPalConfiguration->getButtonConfiguration(),
@@ -1121,6 +1122,8 @@ class Ps_checkout extends PaymentModule
                 'checkout.payment.token.delete.modal.content' => $this->l('The following payment method will be deleted from your account:'),
                 'checkout.payment.token.delete.modal.confirm-button' => $this->l('Delete payment method'),
                 'checkout.payment.loader.processing-request' => $this->l('Please wait, we are processing your request'),
+                'APPLE_PAY_MERCHANT_SESSION_VALIDATION_ERROR' => $this->l('We’re unable to process your Apple Pay payment at the moment. This could be due to an issue verifying the payment setup for this website. Please try again later or choose a different payment method.'),
+                'APPROVE_APPLE_PAY_VALIDATION_ERROR' => $this->l('We encountered an issue while processing your Apple Pay payment. Please verify your order details and try again, or use a different payment method.')
             ],
         ]);
 
