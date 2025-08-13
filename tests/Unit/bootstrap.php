5a70609c1937f28a346ecc4276c606a40f98420d
L3RAZ <laurynas.sedys@invertus.eu>
2025-08-13 15:30:30 +0300

    Clean old files

--- tests/Unit/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$rootDirectory = getenv('_PS_ROOT_DIR_') ?: __DIR__ . '/../../../../';
-$projectDir = __DIR__ . '/../../';
-require_once $rootDirectory . 'config/config.inc.php';
-require_once $projectDir . 'vendor/autoload.php';
