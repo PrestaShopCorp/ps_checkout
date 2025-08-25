109c27db7786be758ac58c72d5a90063c2af5255
L3RAZ <laurynas.sedys@invertus.eu>
2025-08-25 10:37:52 +0300

    Wrong config class name fix

--- api/src/Http/Configuration/PaymentConfigurationBuilder.php
+++ api/src/Http/Configuration/PaymentConfigurationBuilder.php
@@ -32,7 +32,7 @@ use PsCheckout\Infrastructure\Environment\EnvInterface;
 use PsCheckout\Infrastructure\Repository\PsAccountRepository;
 use Psr\Log\LoggerInterface;
 
-class PaymentHttpClientConfigurationBuilder implements HttpClientConfigurationBuilderInterface
+class PaymentConfigurationBuilder implements HttpClientConfigurationBuilderInterface
 {
     const TIMEOUT = 10;
 
--- api/src/Http/Configuration/WebhookConfigurationBuilder.php
+++ api/src/Http/Configuration/WebhookConfigurationBuilder.php
@@ -32,7 +32,7 @@ use PsCheckout\Infrastructure\Environment\EnvInterface;
 use PsCheckout\Infrastructure\Repository\PsAccountRepository;
 use Psr\Log\LoggerInterface;
 
-class WebhookHttpClientConfigurationBuilder implements HttpClientConfigurationBuilderInterface
+class WebhookConfigurationBuilder implements HttpClientConfigurationBuilderInterface
 {
     const TIMEOUT = 10;
 
