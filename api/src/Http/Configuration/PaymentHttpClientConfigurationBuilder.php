9e80aaf12c5d3238b0bc1bca3e07d58332e37ed1
Matt75 <Matt75@users.noreply.github.com>
2025-10-03 14:13:27 +0000

    Apply PHP-CS-Fixer changes

--- api/src/Http/Configuration/PaymentHttpClientConfigurationBuilder.php
+++ api/src/Http/Configuration/PaymentHttpClientConfigurationBuilder.php
@@ -152,6 +152,7 @@ class PaymentHttpClientConfigurationBuilder implements HttpClientConfigurationBu
         $b = random_bytes(16);
         $b[6] = chr(ord($b[6]) & 0x0f | 0x40);
         $b[8] = chr(ord($b[8]) & 0x3f | 0x80);
+
         return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($b), 4));
     }
 }
