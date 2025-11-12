82dfab4d7166e6774b53e9ea0e038b4d42872c54
L3RAZ <laurynas.sedys@invertus.eu>
2025-11-12 16:44:02 +0200

    Added step for creating test database

---
+++ ps8/tests/create-test-database.php
@@ -0,0 +1,35 @@
+<?php
+
+$rootDirectory = __DIR__ . '/../../../';
+$projectDir = __DIR__ . '/../';
+
+require_once $projectDir . 'vendor/autoload.php';
+require_once $rootDirectory . 'config/config.inc.php';
+
+if (file_exists($rootDirectory . 'vendor/autoload.php')) {
+    require_once $rootDirectory . 'vendor/autoload.php';
+}
+
+if (file_exists($rootDirectory . 'autoload.php')) {
+    require_once $rootDirectory . 'autoload.php';
+}
+
+$sourceHost = defined('_DB_SERVER_') ? _DB_SERVER_ : 'mysql';
+$sourceUser = defined('_DB_USER_') ? _DB_USER_ : 'root';
+$sourcePassword = defined('_DB_PASSWD_') ? _DB_PASSWD_ : 'prestashop';
+$sourceDatabase = defined('_DB_NAME_') ? _DB_NAME_ : 'prestashop';
+
+// Target (new) database details
+$targetDatabase = 'test_' . $sourceDatabase;
+
+$createDatabaseCommand = "mysql -h$sourceHost -u$sourceUser -p$sourcePassword -e 'DROP DATABASE IF EXISTS `" . $targetDatabase . '`; CREATE DATABASE `' . $targetDatabase . "`'";
+$importCommand = "mysqldump -h$sourceHost -u" . $sourceUser . " -p'" . $sourcePassword . "' " . $sourceDatabase . " | mysql -h$sourceHost -u " . $sourceUser . " --password='" . $sourcePassword . "' " . $targetDatabase;
+
+echo 'Step 1: Creating New Database' . PHP_EOL;
+echo shell_exec($createDatabaseCommand) . PHP_EOL;
+
+echo 'Step 2: Importing into New Database' . PHP_EOL;
+echo shell_exec($importCommand) . PHP_EOL;
+
+//shell_exec($createDatabaseCommand);
+//shell_exec($importCommand);
