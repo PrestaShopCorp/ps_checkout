3359dacd7b19e784f30cf28f67160ea63657c475
Gaël Reyrol <me@gaelreyrol.dev>
2026-01-26 16:38:18 +0100

    chore: apply reviews

--- core/src/Settings/Configuration/DefaultConfiguration.php
+++ core/src/Settings/Configuration/DefaultConfiguration.php
@@ -49,6 +49,6 @@ class DefaultConfiguration
         'PS_CHECKOUT_DISPLAY_LOGO_CART' => '1',
         'PS_CHECKOUT_HOSTED_FIELDS_CONTINGENCIES' => 'SCA_WHEN_REQUIRED',
         'PS_CHECKOUT_PAYPAL_BUTTON' => '{"shape":"pill","label":"pay","color":"gold"}',
-        'PS_CHECKOUT_ORDER_STATUS_AUTH_CAPTURE' => '',
+        'PS_CHECKOUT_AUTHORIZE_STATES' => '',
     ];
 }
