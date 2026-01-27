b8e8bd96276faf94f38954476658327c8d59aa63
Laurynas Šedys <laurynas.sedys.96@gmail.com>
2026-01-27 11:48:55 +0200

    [PAYSHIP-3554] BNPL Configurator / Adds new PayLater configuration handling (#1388)
    * Added new PayLater configuration handling and removed old configuration references
    
    * Apply PHP-CS-Fixer changes
    
    * Code review feedback
    
    * Apply PHP-CS-Fixer changes
    
    * Fixed default config
    
    * Removed hardcoded country for debugging
    
    * changed the way json config is handled
    
    * Bumped muodle version
    
    * Bump module version
    
    * Remove useless import
    
    * Improve the PayPalPayLaterConfiguration
    
    * Added validation for merchant country/locale/currency
    
    * Apply PHP-CS-Fixer changes
    
    * Updated default configuration to enable messages
    
    * Fixed upgrade file config name
    
    ---------
    
    Co-authored-by: L3RAZ <L3RAZ@users.noreply.github.com>
    Co-authored-by: Matt75 <5262628+Matt75@users.noreply.github.com>

--- core/src/FundingSource/Constraint/FundingSourceConstraint.php
+++ core/src/FundingSource/Constraint/FundingSourceConstraint.php
@@ -38,7 +38,7 @@ class FundingSourceConstraint
             'ideal' => ['NL'],
             'mybank' => ['IT'],
             'p24' => ['PL'],
-            'paylater' => ['FR', 'GB', 'US', 'ES', 'IT'],
+            'paylater' => ['AU', 'DE', 'ES', 'FR', 'GB', 'IT', 'US'],
             'google_pay' => ['AU', 'AT', 'BE', 'BG', 'CA', 'CN', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HK', 'HU', 'IE', 'IT', 'JP', 'LV', 'LI', 'LT', 'LU', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'SG', 'SK', 'SI', 'ES', 'SE', 'GB', 'US'],
             'apple_pay' => ['AU', 'AT', 'BE', 'BG', 'CA', 'CN', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HK', 'HU', 'IE', 'IT', 'JP', 'LV', 'LI', 'LT', 'LU', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'SG', 'SK', 'SI', 'ES', 'SE', 'GB', 'US'],
         ];
@@ -58,6 +58,7 @@ class FundingSourceConstraint
         $currencies = [
             'google_pay' => ['AUD', 'BRL', 'CAD', 'CHF', 'CZK', 'DKK', 'EUR', 'GBP', 'HKD', 'HUF', 'ILS', 'JPY', 'MXN', 'NOK', 'NZD', 'PHP', 'PLN', 'SEK', 'SGD', 'THB', 'TWD', 'USD'],
             'apple_pay' => ['AUD', 'BRL', 'CAD', 'CHF', 'CZK', 'DKK', 'EUR', 'GBP', 'HKD', 'HUF', 'ILS', 'JPY', 'MXN', 'NOK', 'NZD', 'PHP', 'PLN', 'SEK', 'SGD', 'THB', 'TWD', 'USD'],
+            'paylater' => ['EUR', 'USD', 'AUD', 'GBP'],
         ];
 
         return isset($currencies[$fundingSourceName]) ? $currencies[$fundingSourceName] : [];
