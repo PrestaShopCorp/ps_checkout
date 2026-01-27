2cb3fa62bda7b31edaadfd5281a3d270e0111e6c
Laurynas Šedys <laurynas.sedys.96@gmail.com>
2026-01-27 11:48:55 +0200

    [PAYSHIP-3554] BNPL Configurator / Adds new PayLater configuration handling (#1388)
    * Added new PayLater configuration handling and removed old configuration references
    
    * Apply PHP-CS-Fixer changes
    
    * Code review feedback
    
    * Apply PHP-CS-Fixer changes
    
    * Fixed default config
    
    * Removed hardcoded country for debugging
    
    * changed the way json config is handled
    
    * Bumped muodle version
    
    * Bump module version
    
    * Remove useless import
    
    * Improve the PayPalPayLaterConfiguration
    
    * Added validation for merchant country/locale/currency
    
    * Apply PHP-CS-Fixer changes
    
    * Updated default configuration to enable messages
    
    * Fixed upgrade file config name
    
    ---------
    
    Co-authored-by: L3RAZ <L3RAZ@users.noreply.github.com>
    Co-authored-by: Matt75 <5262628+Matt75@users.noreply.github.com>

---
+++ infrastructure/src/Validator/PayLaterValidator.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace PsCheckout\Infrastructure\Validator;
+
+use PsCheckout\Core\FundingSource\Constraint\FundingSourceConstraint;
+use PsCheckout\Core\Settings\Configuration\PayPalConfiguration;
+use PsCheckout\Infrastructure\Adapter\ContextInterface;
+
+class PayLaterValidator implements PayLaterValidatorInterface
+{
+    /**
+     * @var ContextInterface
+     */
+    private $context;
+
+    /**
+     * @var PayPalConfiguration
+     */
+    private $payPalConfiguration;
+
+    public function __construct(ContextInterface $context, PayPalConfiguration $payPalConfiguration)
+    {
+        $this->context = $context;
+        $this->payPalConfiguration = $payPalConfiguration;
+    }
+
+    public function isPayLaterAvailable(): bool
+    {
+        $merchantCountry = $this->payPalConfiguration->getMerchantCountry();
+        $countries = FundingSourceConstraint::getCountries('paylater');
+        $currency = $this->context->getCurrency()->iso_code;
+        $locale = $this->context->getLanguage()->locale;
+        $customerCountry = $this->context->getCountry()->iso_code;
+
+        // Define supported country-currency combinations for Pay Later messaging
+        $supportedCountryCurrencyMap = [
+            'AU' => 'AUD', // Australia
+            'FR' => 'EUR', // France
+            'DE' => 'EUR', // Germany
+            'IT' => 'EUR', // Italy
+            'ES' => 'EUR', // Spain
+            'GB' => 'GBP', // United Kingdom
+            'US' => 'USD', // United States
+        ];
+
+        // Define locale to country mapping for website locale validation
+        $localeCountryMap = [
+            'en-AU' => 'AU', // Australia
+            'fr-FR' => 'FR', // France
+            'de-DE' => 'DE', // Germany
+            'it-IT' => 'IT', // Italy
+            'es-ES' => 'ES', // Spain
+            'en-GB' => 'GB', // United Kingdom
+            'en-US' => 'US', // United States
+        ];
+
+        return in_array($merchantCountry, $countries, true)
+            && $merchantCountry === $customerCountry
+            && isset($supportedCountryCurrencyMap[$customerCountry])
+            && $supportedCountryCurrencyMap[$customerCountry] === $currency
+            && isset($localeCountryMap[$locale])
+            && $localeCountryMap[$locale] === $merchantCountry;
+    }
+}
---
+++ infrastructure/tests/Unit/Validator/PayLaterValidatorTest.php
@@ -0,0 +1,272 @@
+<?php
+
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+namespace Tests\Unit\PsCheckout\Infrastructure\Validator;
+
+use PHPUnit\Framework\TestCase;
+use PsCheckout\Core\Settings\Configuration\PayPalConfiguration;
+use PsCheckout\Infrastructure\Adapter\Context;
+use PsCheckout\Infrastructure\Validator\PayLaterValidator;
+
+class PayLaterValidatorTest extends TestCase
+{
+    private $contextMock;
+
+    private $payPalConfigurationMock;
+
+    private $payLaterValidator;
+
+    protected function setUp(): void
+    {
+        $this->contextMock = $this->createMock(Context::class);
+        $this->payPalConfigurationMock = $this->createMock(PayPalConfiguration::class);
+        $this->payLaterValidator = new PayLaterValidator($this->contextMock, $this->payPalConfigurationMock);
+    }
+
+    /**
+     * @dataProvider validPayLaterScenariosProvider
+     */
+    public function testIsPayLaterAvailableReturnsTrueForValidScenarios(
+        string $merchantCountry,
+        string $customerCountry,
+        string $currency,
+        string $locale
+    ): void {
+        $this->payPalConfigurationMock
+            ->method('getMerchantCountry')
+            ->willReturn($merchantCountry);
+
+        $currencyMock = $this->createMock(\stdClass::class);
+        $currencyMock->iso_code = $currency;
+
+        $languageMock = $this->createMock(\stdClass::class);
+        $languageMock->locale = $locale;
+
+        $countryMock = $this->createMock(\stdClass::class);
+        $countryMock->iso_code = $customerCountry;
+
+        $this->contextMock
+            ->method('getCurrency')
+            ->willReturn($currencyMock);
+
+        $this->contextMock
+            ->method('getLanguage')
+            ->willReturn($languageMock);
+
+        $this->contextMock
+            ->method('getCountry')
+            ->willReturn($countryMock);
+
+        $this->assertTrue($this->payLaterValidator->isPayLaterAvailable());
+    }
+
+    /**
+     * @dataProvider invalidPayLaterScenariosProvider
+     */
+    public function testIsPayLaterAvailableReturnsFalseForInvalidScenarios(
+        string $merchantCountry,
+        string $customerCountry,
+        string $currency,
+        string $locale
+    ): void {
+        $this->payPalConfigurationMock
+            ->method('getMerchantCountry')
+            ->willReturn($merchantCountry);
+
+        $currencyMock = $this->createMock(\stdClass::class);
+        $currencyMock->iso_code = $currency;
+
+        $languageMock = $this->createMock(\stdClass::class);
+        $languageMock->locale = $locale;
+
+        $countryMock = $this->createMock(\stdClass::class);
+        $countryMock->iso_code = $customerCountry;
+
+        $this->contextMock
+            ->method('getCurrency')
+            ->willReturn($currencyMock);
+
+        $this->contextMock
+            ->method('getLanguage')
+            ->willReturn($languageMock);
+
+        $this->contextMock
+            ->method('getCountry')
+            ->willReturn($countryMock);
+
+        $this->assertFalse($this->payLaterValidator->isPayLaterAvailable());
+    }
+
+    public function testIsPayLaterAvailableReturnsFalseForUnsupportedMerchantCountry(): void
+    {
+        $this->payPalConfigurationMock
+            ->method('getMerchantCountry')
+            ->willReturn('CA'); // Canada is not supported
+
+        $currencyMock = $this->createMock(\stdClass::class);
+        $currencyMock->iso_code = 'CAD';
+
+        $languageMock = $this->createMock(\stdClass::class);
+        $languageMock->locale = 'en-CA';
+
+        $countryMock = $this->createMock(\stdClass::class);
+        $countryMock->iso_code = 'CA';
+
+        $this->contextMock
+            ->method('getCurrency')
+            ->willReturn($currencyMock);
+
+        $this->contextMock
+            ->method('getLanguage')
+            ->willReturn($languageMock);
+
+        $this->contextMock
+            ->method('getCountry')
+            ->willReturn($countryMock);
+
+        $this->assertFalse($this->payLaterValidator->isPayLaterAvailable());
+    }
+
+    public function testIsPayLaterAvailableReturnsFalseForMismatchedMerchantAndCustomerCountry(): void
+    {
+        $this->payPalConfigurationMock
+            ->method('getMerchantCountry')
+            ->willReturn('US'); // US merchant
+
+        $currencyMock = $this->createMock(\stdClass::class);
+        $currencyMock->iso_code = 'USD';
+
+        $languageMock = $this->createMock(\stdClass::class);
+        $languageMock->locale = 'en-US';
+
+        $countryMock = $this->createMock(\stdClass::class);
+        $countryMock->iso_code = 'CA'; // Canadian customer
+
+        $this->contextMock
+            ->method('getCurrency')
+            ->willReturn($currencyMock);
+
+        $this->contextMock
+            ->method('getLanguage')
+            ->willReturn($languageMock);
+
+        $this->contextMock
+            ->method('getCountry')
+            ->willReturn($countryMock);
+
+        $this->assertFalse($this->payLaterValidator->isPayLaterAvailable());
+    }
+
+    public function testIsPayLaterAvailableReturnsFalseForWrongCurrency(): void
+    {
+        $this->payPalConfigurationMock
+            ->method('getMerchantCountry')
+            ->willReturn('US');
+
+        $currencyMock = $this->createMock(\stdClass::class);
+        $currencyMock->iso_code = 'EUR'; // Wrong currency for US
+
+        $languageMock = $this->createMock(\stdClass::class);
+        $languageMock->locale = 'en-US';
+
+        $countryMock = $this->createMock(\stdClass::class);
+        $countryMock->iso_code = 'US';
+
+        $this->contextMock
+            ->method('getCurrency')
+            ->willReturn($currencyMock);
+
+        $this->contextMock
+            ->method('getLanguage')
+            ->willReturn($languageMock);
+
+        $this->contextMock
+            ->method('getCountry')
+            ->willReturn($countryMock);
+
+        $this->assertFalse($this->payLaterValidator->isPayLaterAvailable());
+    }
+
+    public function testIsPayLaterAvailableReturnsFalseForUnsupportedLocale(): void
+    {
+        $this->payPalConfigurationMock
+            ->method('getMerchantCountry')
+            ->willReturn('US');
+
+        $currencyMock = $this->createMock(\stdClass::class);
+        $currencyMock->iso_code = 'USD';
+
+        $languageMock = $this->createMock(\stdClass::class);
+        $languageMock->locale = 'fr-FR'; // French locale for US merchant
+
+        $countryMock = $this->createMock(\stdClass::class);
+        $countryMock->iso_code = 'US';
+
+        $this->contextMock
+            ->method('getCurrency')
+            ->willReturn($currencyMock);
+
+        $this->contextMock
+            ->method('getLanguage')
+            ->willReturn($languageMock);
+
+        $this->contextMock
+            ->method('getCountry')
+            ->willReturn($countryMock);
+
+        $this->assertFalse($this->payLaterValidator->isPayLaterAvailable());
+    }
+
+    public function validPayLaterScenariosProvider(): array
+    {
+        return [
+            'Australia scenario' => ['AU', 'AU', 'AUD', 'en-AU'],
+            'France scenario' => ['FR', 'FR', 'EUR', 'fr-FR'],
+            'Germany scenario' => ['DE', 'DE', 'EUR', 'de-DE'],
+            'Italy scenario' => ['IT', 'IT', 'EUR', 'it-IT'],
+            'Spain scenario' => ['ES', 'ES', 'EUR', 'es-ES'],
+            'United Kingdom scenario' => ['GB', 'GB', 'GBP', 'en-GB'],
+            'United States scenario' => ['US', 'US', 'USD', 'en-US'],
+        ];
+    }
+
+    public function invalidPayLaterScenariosProvider(): array
+    {
+        return [
+            'Wrong currency for Australia' => ['AU', 'AU', 'USD', 'en-AU'],
+            'Wrong currency for France' => ['FR', 'FR', 'USD', 'fr-FR'],
+            'Wrong currency for Germany' => ['DE', 'DE', 'GBP', 'de-DE'],
+            'Wrong currency for Italy' => ['IT', 'IT', 'AUD', 'it-IT'],
+            'Wrong currency for Spain' => ['ES', 'ES', 'GBP', 'es-ES'],
+            'Wrong currency for UK' => ['GB', 'GB', 'EUR', 'en-GB'],
+            'Wrong currency for US' => ['US', 'US', 'EUR', 'en-US'],
+            'Wrong locale for Australia' => ['AU', 'AU', 'AUD', 'fr-FR'],
+            'Wrong locale for France' => ['FR', 'FR', 'EUR', 'en-US'],
+            'Wrong locale for Germany' => ['DE', 'DE', 'EUR', 'it-IT'],
+            'Wrong locale for Italy' => ['IT', 'IT', 'EUR', 'es-ES'],
+            'Wrong locale for Spain' => ['ES', 'ES', 'EUR', 'de-DE'],
+            'Wrong locale for UK' => ['GB', 'GB', 'GBP', 'fr-FR'],
+            'Wrong locale for US' => ['US', 'US', 'USD', 'de-DE'],
+            'Unsupported merchant country' => ['CA', 'CA', 'CAD', 'en-CA'],
+            'Unsupported customer country' => ['US', 'CA', 'USD', 'en-US'],
+        ];
+    }
+}
