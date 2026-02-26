bb0df09cc09b9be1373fb67ea4a6e8f1dc5528bb
Laurynas Šedys <laurynas.sedys.96@gmail.com>
2026-02-26 10:45:14 +0200

    Added missing Pay upon Invoice translations (#1466)
    Adds missing Pay upon invoice translations

--- ps17/src/Presentation/Translator.php
+++ ps17/src/Presentation/Translator.php
@@ -78,6 +78,8 @@ class Translator implements TranslatorInterface
             'Pay in installments with PayPal Pay Later' => $this->module->l('Pay in installments with PayPal Pay Later', 'Translator'),
             'Pay by %s' => $this->module->l('Pay by %s', 'Translator'),
             'Pay with %s' => $this->module->l('Pay with %s', 'Translator'),
+            'Pay later with invoice' => $this->module->l('Pay later with invoice', 'Translator'),
+            'Pay upon Invoice' => $this->module->l('Pay upon Invoice', 'Translator'),
 
             'Card holder name' => $this->module->l('Card holder name', 'Translator'),
             'Card number' => $this->module->l('Card number', 'Translator'),
