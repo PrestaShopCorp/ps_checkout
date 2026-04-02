c8b8777fb5f95f1cecb2b6f84f8572eaf30bf271
Laurynas Šedys <laurynas.sedys.96@gmail.com>
2026-04-02 17:16:51 +0300

    [PAYSHIP-3992] upgrade scripts (#1501)
    * Adds back 5.2.0 upgrade
    
    * Added voided order state creation

--- ps17/upgrade/upgrade-7.5.3.0.php
+++ ps17/upgrade/upgrade-7.5.3.0.php
@@ -33,12 +33,125 @@ function upgrade_module_7_5_3_0(Ps_checkout $module)
     try {
         $db = Db::getInstance();
 
-    } catch (Exception $exception) {
+        // Check columns in pscheckout_authorization table
+        $fields = $db->executeS('SHOW COLUMNS FROM `' . _DB_PREFIX_ . 'pscheckout_authorization`');
 
+        if (!empty($fields)) {
+            $hasCreateTime = false;
+            $hasUpdateTime = false;
+            $hasSellerProtection = false;
+
+            foreach ($fields as $field) {
+                if ($field['Field'] === 'create_time') {
+                    $hasCreateTime = true;
+                }
+                if ($field['Field'] === 'update_time') {
+                    $hasUpdateTime = true;
+                }
+                if ($field['Field'] === 'seller_protection') {
+                    $hasSellerProtection = true;
+                }
+            }
+
+            // Add create_time column if it doesn't exist
+            if (!$hasCreateTime) {
+                $db->execute('
+                    ALTER TABLE `' . _DB_PREFIX_ . 'pscheckout_authorization`
+                    ADD COLUMN `create_time` varchar(20) NOT NULL DEFAULT ""
+                ');
+            }
+
+            // Add update_time column if it doesn't exist
+            if (!$hasUpdateTime) {
+                $db->execute('
+                    ALTER TABLE `' . _DB_PREFIX_ . 'pscheckout_authorization`
+                    ADD COLUMN `update_time` varchar(20) NOT NULL DEFAULT ""
+                ');
+            }
+
+            // Drop seller_protection column if it exists
+            if ($hasSellerProtection) {
+                $db->execute('
+                    ALTER TABLE `' . _DB_PREFIX_ . 'pscheckout_authorization`
+                    DROP COLUMN `seller_protection`
+                ');
+            }
+        }
+    } catch (Exception $exception) {
         PrestaShopLogger::addLog($exception->getMessage(), 4, 1, 'Module', $module->id);
 
         return false;
     }
 
+    // Install voided order state if not already present
+    try {
+        $voidedStateId = (int) Configuration::getGlobalValue('PS_CHECKOUT_STATE_VOIDED');
+        $needsCreation = true;
+
+        if ($voidedStateId) {
+            $orderState = new OrderState($voidedStateId);
+            if ($orderState->id && $orderState->module_name === $module->name && !$orderState->deleted) {
+                $needsCreation = false;
+            }
+        }
+
+        if ($needsCreation) {
+            ps_checkout_create_order_state_7_5_3_0(
+                'PS_CHECKOUT_STATE_VOIDED',
+                '#DC143C',
+                [
+                    'en' => 'Authorization voided',
+                    'fr' => 'Autorisation annulée',
+                    'es' => 'Autorización anulada',
+                    'it' => 'Autorizzazione annullata',
+                    'nl' => 'Autorisatie ongeldig gemaakt',
+                    'de' => 'Autorisierung aufgehoben',
+                    'pl' => 'Autoryzacja unieważniona',
+                    'pt' => 'Autorização anulada',
+                ]
+            );
+        }
+    } catch (Exception $exception) {
+        PrestaShopLogger::addLog($exception->getMessage(), 4, 1, 'Module', $module->id);
+
+        return false;
+    }
+
+    $module->registerHook('actionOrderStatusPostUpdate');
+
     return true;
+}
+
+function ps_checkout_create_order_state_7_5_3_0($configuration_key, $color, $nameByLangIsoCode)
+{
+    $orderStateNameByLangId = [];
+
+    foreach (Language::getLanguages(false) as $language) {
+        $languageIsoCode = Tools::strtolower($language['iso_code']);
+
+        if (isset($nameByLangIsoCode[$languageIsoCode])) {
+            $orderStateNameByLangId[(int) $language['id_lang']] = $nameByLangIsoCode[$languageIsoCode];
+        } elseif (isset($nameByLangIsoCode['en'])) {
+            $orderStateNameByLangId[(int) $language['id_lang']] = $nameByLangIsoCode['en'];
+        }
+    }
+
+    $orderState = new OrderState();
+    $orderState->name = $orderStateNameByLangId;
+    $orderState->module_name = 'ps_checkout';
+    $orderState->unremovable = true;
+    $orderState->color = $color;
+    $orderState->delivery = false;
+    $orderState->shipped = false;
+    $orderState->pdf_delivery = false;
+    $orderState->pdf_invoice = false;
+    $orderState->hidden = false;
+    $orderState->invoice = false;
+    $orderState->send_email = false;
+    $orderState->paid = false;
+    $orderState->logable = false;
+    $orderState->deleted = false;
+    $orderState->template = [];
+    $orderState->save();
+    Configuration::updateGlobalValue($configuration_key, $orderState->id);
 }
